cf8396eeba49620bb59e7d71f983b40fb6a66db3
Jermaine Junior <obietrice734@gmail.com>
2024-11-30 11:20:16 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup></script>
+<template>
+    <div class="container m-6">
+        <section>
+            <header class="">
+                <ul
+                    class="flex items-center justify-center space-x-6 list-none text-sm"
+                >
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </header>
+        </section>
+        <main>
+            <div class="py-16">
+                <div class="flex">
+                    <p class="text-xl">main Content</p>
+                </div>
+            </div>
+        </main>
+    </div>
+</template>
+
