de744df205d628b48f69fe0de9d0d50cca601841
Jermaine Junior <obietrice734@gmail.com>
2024-12-01 08:57:32 +0200

    setup pinia and modified the server config

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import router from './router'
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(createPinia).use(router).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,16 @@ export default defineConfig({
   ],
   server: {
     port: 3000,
+    proxy:{
+      '/api': {
+        target: "http://localhost:8000",
+        changeOrigin : true,
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      },
+    },
   },
   resolve: {
     alias: {
